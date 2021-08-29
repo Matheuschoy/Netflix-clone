dc91ed114b5b49fba51d6567ec2c933178f6c807
Matheus Choynowski <matheuschoy@gmail.com>
2021-08-29 19:19:58 -0300

    added projeto-netflix

---
+++ Areausuario/Area2/Js owl/Setup.js
@@ -0,0 +1,22 @@
+$('.owl-carousel').owlCarousel({
+    loop:false, //efeito de loop infinito
+    margin:10, //MARGEM ENTRE UM E OUTRO
+    nav:false,  //botos de navegacao
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+document.getElementsByClassName("botao")
+
+function descpeaky () {
+    window.location.href = "/Areausuario/Area2/Area3descricoes/peaky.html";
+}
---
+++ Areausuario/areajs.js
@@ -0,0 +1,8 @@
+function gerenciar () {
+    window.alert("GERENCIAR PERFIS ainda não está disponivel!")
+}
+
+function clicar () {
+    window.location.href = "/Areausuario/Area2/Index.html";
+}
+
